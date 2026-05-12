b8e9caf8a53e149a046054d5dfa988d35a2f4ce5
Nidhi <mrsnidhi2005@gmail.com>
2026-05-12 12:13:55 +0530

    first commit

---
+++ cubeofanum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,cube;
+    cout << "Enter a number : ";
+    cin >> n;
+    cube = n*n*n;
+    cout << "Cube = " <<cube;
+    return 0;
+} 
